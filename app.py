96144ba245776df9a5bf448c142a1810ed788ec1
Kittiiya <91486494+Kittiiya@users.noreply.github.com>
2021-09-28 00:06:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok" 
+    return "Hello นางสาวกฤติยา ทองศรีสมบูรณ์ เลขที่5 ม.4/8" 
 
 @app.route('/api', methods=['GET'])
 def get_api():
